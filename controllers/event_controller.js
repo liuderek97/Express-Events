456a32d55d9b97abb0605188ed3e50a500a2c811
Ewe Lin Loo <ewelinloo@gmail.com>
2019-10-21 15:23:13 +1100

    Add events index

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -1,7 +1,16 @@
 const EventModel = require("./../database/models/event_model");
 
-const show = (req, res) => {
-    res.render("events/index");
+const index = async (req, res) => {
+    let events = await EventModel.find();
+    res.render("events/index", { events });
+}
+
+const show = async (req, res) => {
+    let { id } = req.params;
+    let event = await EventModel.findById(id)
+        .catch(err => res.status(500).send(err))
+
+    res.render("events/show", {event});
 }
 
 const render = (req, res) => {
@@ -12,11 +21,12 @@ const create = async (req, res) => {
     let {title, eventDate, description} = req.body;
     let event = await EventModel.create({title, eventDate, description})
         .catch(err => res.status(500).send(err));
-    res.render(`events/${events._id}`);
+    res.redirect(`/events`);
 }
 
 module.exports = {
+    index,
     show,
-    create,
-    render
+    render,
+    create
 }
