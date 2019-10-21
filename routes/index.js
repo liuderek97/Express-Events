7882ee89456e250710d6f8fc658354afd709a7d7
Ewe Lin Loo <ewelinloo@gmail.com>
2019-10-21 12:44:32 +1100

    Add events controller, route and view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const EventController = require("./../controllers/event_controller");
 
 router.get("/", (req, res) => res.send("Welcome"));
+
+router.get("/events", EventController.show);
 
 module.exports = router;
